ac5180a8a8fa468ab64c8f84979845affca264f2
navining <superokvv@gmail.com>
2020-12-31 02:26:20 +0800

    add pages into bag

--- bot/src/plugins/game/kernel/bag.py
+++ bot/src/plugins/game/kernel/bag.py
@@ -1,4 +1,5 @@
 import random
+import math
 from nonebot import on_startswith
 from nonebot.rule import to_me
 from nonebot.adapters.cqhttp import Bot, Event
@@ -11,14 +12,38 @@ from game.utils.text2image import toImage
 
 user_bag = on_startswith(msg="背包", rule=to_me(), priority=1)
 
+
 @user_bag.handle()
 async def user_bag_handler(bot: Bot, event: Event, state: dict):
-  user = check_account(event)
-  bag = Bag.getBagByUser(user)
-  ret = ""
-  if (bag != None):
-    for (id, player) in bag.players:
-      ret += "[" + str(id) + "]\t"
-      ret += player.format()
-      ret += "\n"
-  await user_bag.finish("当前背包：\n" + toImage(ret), **{"at_sender": True})
+    arg = str(event.message).split(" ", 1)
+    user = check_account(event)
+    bag = Bag.getBagByUser(user)
+    if (len(arg) == 1):
+        await get_bag_by_page(bag, "1")
+    else:
+        page = arg[1]
+        if not page.isdecimal():
+            await user_bag.finish("参数错误", **{"at_sender": True})
+            return
+        await get_bag_by_page(bag, page)
+
+
+async def get_bag_by_page(bag: Bag, page: str):
+    total_page = math.ceil(len(bag.players) / 20)
+    page = int(page)
+    if page > total_page or page <= 0:
+        await user_bag.finish("页码错误", **{"at_sender": True})
+        return
+
+    ret = ""
+    if (bag != None):
+        for i in range(20):
+            index = (page - 1) * 20 + i
+            if index >= len(bag.players):
+                break
+            (id, player) = bag.players[index]
+            ret += "[" + str(id) + "]\t"
+            ret += player.format()
+            ret += "\n"
+    foot = "第" + str(page) + "页/共" + str(total_page) + "页\n请输入“背包 页码”跳转到指定页"
+    await user_bag.finish("当前背包：\n" + toImage(ret+foot), **{"at_sender": True})
