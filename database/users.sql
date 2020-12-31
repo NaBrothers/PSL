c53d6866d30e3ce0ecea3d5daa6e70f15d85bf35
navining <superokvv@gmail.com>
2020-12-31 19:31:23 +0800

    fix a bug: long QQ number will crash

--- database/users.sql
+++ database/users.sql
@@ -1,8 +1,8 @@
 create table IF NOT EXISTS `users` (
   `ID` smallint(6) NOT NULL auto_increment,
-  `QQ` int(11) NOT NULL UNIQUE,
+  `QQ` bigint NOT NULL UNIQUE,
   `Name` varchar(50) default null,
   `Level` int(11) default null,
-  `Money` int(11) default null
-  primary key(`id`)
+  `Money` int(11) default null,
+  primary key(`ID`)
 ) default charset = utf8;
