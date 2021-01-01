c76c4fc691da64f6b000753e65b6825a892858b8
navining <superokvv@gmail.com>
2021-01-01 16:24:26 +0800

    modify sort algorithm in bag

--- bot/src/plugins/game/model/bag.py
+++ bot/src/plugins/game/model/bag.py
@@ -12,5 +12,5 @@ class Bag:
     stars = [i[3] for i in data]
     styles = [i[4] for i in data]
     self.cards = [Card(players[i], user, stars[i], styles[i], ids[i]) for i in range(len(data))]
-    self.cards.sort(key = lambda p : p.player.Overall, reverse=True)
+    self.cards.sort(key = lambda p : (p.player.Overall, p.player.Name), reverse=True)
     
