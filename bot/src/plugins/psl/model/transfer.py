135e90590d0c0cffdd1107d5ba7f459bdb99ba27
Navi Ning <navi@localhost.localdomain>
2022-03-23 20:04:19 +0800

    bugfix

--- bot/src/plugins/psl/model/transfer.py
+++ bot/src/plugins/psl/model/transfer.py
@@ -11,5 +11,5 @@ class Transfer:
     self.cost = data[3]
 
   def format(self):
-    return self.user.name + " [" + str(self.card.id) + "] " + self.card.player.Position+"\t" + self.card.getNameWithColor() + " " + str(self.card.overall) + " " + Const.STARS[self.card.star]["star"] + " ◆+" + str(self.breach)  + " " + self.card.getStyle() + " " + Card.formatPrice(self.cost)
+    return self.user.name + " [" + str(self.card.id) + "] " + self.card.player.Position+"\t" + self.card.getNameWithColor() + " " + str(self.card.overall) + " " + Const.STARS[self.card.star]["star"] + " ◆+" + str(self.card.breach)  + " " + self.card.getStyle() + " " + Card.formatPrice(self.cost)
     
